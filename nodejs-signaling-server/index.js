0eeee579124fa9980d6c3d0299676818c7466ed1
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-12-05 14:45:18 -0300

    Criado mapa de clients e sendMenssageToClient

--- nodejs-signaling-server/index.js
+++ nodejs-signaling-server/index.js
@@ -1,5 +1,5 @@
 import { customAlphabet } from "nanoid";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const nanoid = customAlphabet(alphabet, 4);
@@ -8,6 +8,9 @@ const db = {
   // answers: {},// mapa: clientID -> answer
 };
 
+/** @type {Map<string, WebSocket} */
+const clients = new Map();
+
 const PORT = 8989;
 const wss = new WebSocketServer({ port: PORT, path: "/ws/signaling-server" });
 
@@ -15,9 +18,11 @@ wss.on("connection", (ws, req) => {
   const clientData = {
     clientId: nanoid(),
   };
+  clients.set(clientData.clientId, ws);
 
   ws.on("close", (code, reason) => {
     console.log("close code: %s, code: %s", code, reason);
+    clients.delete(clientData.clientId);
   });
 
   ws.on("error", (error) => {
@@ -39,12 +44,27 @@ wss.on("connection", (ws, req) => {
     switch (message.type) {
       case "rtc-session-description-offer":
         db.offers[clientData.clientId] = message.data;
+        console.dir(db);
+        break;
+      case "get-rtc-session-description-offer":
+        ws.send(
+          JSON.stringify({
+            type: "res-get-rtc-session-description-offer",
+            data: db.offers[message.data.clientId],
+          })
+        );
+        break;
+      case "rtc-session-description-answer":
+        const dataToClient = {
+          type: "rtc-session-description-answer",
+          data: message.data.rtcSessionDescriptionAnswer,
+        };
+        sendMenssageToClient(message.data.clientIdOffer, dataToClient);
         break;
       default:
         console.warn("%s message desconhecida:", clientData.clientId, message);
         break;
     }
-    console.dir(db);
   });
   // ws.on("ping", (data) => {
   //   console.log("ping", data);
@@ -76,3 +96,12 @@ wss.on("connection", (ws, req) => {
 wss.on("listening", () => {
   console.log(`Signaling server em execução: ${PORT}`);
 });
+
+function sendMenssageToClient(clientId, data) {
+  if (!clients.has(clientId)) {
+    console.warn("Não existe o cliente", clientId);
+    return;
+  }
+
+  clients.get(clientId).send(JSON.stringify(data));
+}
